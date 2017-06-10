ddd5d30e4767deac3018cc8ce2dc304ab6aae3c0
jogging20 <621644@qq.com>
2017-06-10 13:21:56 +0800

    5

---
+++ leetcode/easy/leetCode/2 Two sum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2 Two sum
+//
+//  Created by kqy on 10/06/2017.
+//  Copyright © 2017 KQY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
