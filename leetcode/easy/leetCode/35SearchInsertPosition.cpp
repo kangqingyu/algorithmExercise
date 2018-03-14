7a2e4af42117cd20dd64e369a40a0c7cc40c219d
kqy <621644@qq.com>
2018-03-14 16:08:14 +0800

    89

---
+++ leetcode/easy/leetCode/35SearchInsertPosition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+}
+
+int searchInsert(vector<int>& nums, int target) {
+    int i = 0;
+	 while ((target > A[i]) && (i < n)) ++i;
+	  
+	 
+	return i;
+}
+
+ 
